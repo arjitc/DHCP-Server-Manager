1b956d0984d8d0b12f90904685bcb5d240859783
Arjit Chaudhary <arjit.c@gmail.com>
2016-09-22 01:25:21 +0530

    Restart dhcpd

--- add_dhcp_entry.sh
+++ add_dhcp_entry.sh
@@ -5,3 +5,4 @@ ip_address=$3
 subnet=$4
 gateway=$5
 sed -i "/#ipv4/a host $hostname {hardware ethernet $mac_address;fixed-address $ip_address;option subnet-mask $subnet;option routers $gateway;}" /etc/dhcp/dhcpd.conf
+service dhcpd restart
